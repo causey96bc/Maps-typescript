398996c10134de1d99374a52a839055592d32eb3
causey904.bc <your_email@real_email.edu>
2020-12-06 11:43:22 -0500

    adding company class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import {User} from "./User";
+import {Company} from "./Company"}
 const user = new User();
 console.log("user", user);
+ const company = new Company();
+ console.log("company info:", company);;
  
