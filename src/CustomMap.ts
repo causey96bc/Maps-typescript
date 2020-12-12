56251689338dfff1b7e008cca2545b203bd425f8
causey904.bc <your_email@real_email.edu>
2020-12-11 20:45:37 -0500

    dont commit

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,14 +1,6 @@
 import {User} from "./User";
 import {Company} from "./Company";
- 
-//instruction to every other class 
-//on how to be an argument to add marker.
-interface Mappable{
-  location: {
-    lat: number,
-    lng: number,
-  }
- }
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -22,12 +14,21 @@ export class CustomMap {
       }
     });
   }  
-  addMarker(mappable: Mappable): void {
+  addUserMarker(user: User): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: mappable.location.lat,
-        lng: mappable.location.lng,
+        lat: user.location.lat,
+        lng: user.location.lng,
+      }
+    });
+  }
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
       }
     });
   }
